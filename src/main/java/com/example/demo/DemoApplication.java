7709e02e02e073b024bae5118650baa889fec762
pranay_kohad <prakohad@gmail.com>
2024-03-15 11:53:05 +0530

    commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("user")
+public class DemoApplication {
+
+	final static Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Server started...");
+		log.info("info: Server started...");
+		log.debug("debug: Server started...");
+		log.error("error: Server started...");
+	}
+
+	@GetMapping("friends")
+	public List<String> getFriends() {
+		List<String> list = new ArrayList<>();
+		list.add("Pranay Kohad");
+		list.add("Anuj Kumar");
+		list.add("Nilesh Kapale");
+		list.add("Bhushan Patil");
+		return list;
+	}
+}
